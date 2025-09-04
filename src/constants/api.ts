6c858f73e4dbd915ba17cdfea05e57e6240c04f2
pedromartinspe07 <pedromartinspe07@gmail.com>
2025-09-04 11:13:28 -0300

    Atualização 0.1

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -7,4 +7,4 @@
 // Exemplo de URL de produção: 'https://sua-api.com/api/v1'
 
 // Use a porta do seu servidor Express (3000) e inclua o prefixo de versão '/v1'
-export const API_BASE_URL = 'http://localhost:3000/api/v1';
+export const API_BASE_URL = 'http://biosuape-backend-api-production.up.railway.app:3000/api/v1';
